9bf859bd730a627cfc3b67ca32472f9de0b6a610
Marcel Veldhuizen <marcel@subbot.net>
2021-12-17 10:47:11 +0100

    Add initial day 17 solutions
    Part 1 is just bruteforced for now.
    Part 2 is a little smarter, but still has a magic number in there.
    Hopefully I can fix them later :)

---
+++ Thorarin.AdventOfCode/Year2021/Day17A.cs
@@ -0,0 +1,82 @@
+﻿using System.Text.RegularExpressions;
+using Thorarin.AdventOfCode.Framework;
+
+namespace Thorarin.AdventOfCode.Year2021;
+
+[Puzzle(Year = 2021, Day = 17, Part = 1)]
+public class Day17A : Puzzle
+{
+    private int _y1;
+    private int _y2;
+
+    public override Output SampleExpectedOutput => new Answer(45, 9);
+    public override Output ProblemExpectedOutput => new Answer(23005, 214);
+
+    public override void ParseInput(TextReader reader)
+    {
+        var line = reader.ReadLine()!;
+        var match = Regex.Match(line, "^target area: x=(?<x1>-?\\d+)\\.\\.(?<x2>-?\\d+), y=(?<y1>-?\\d+)\\.\\.(?<y2>-?\\d+)");
+
+        _y1 = int.Parse(match.Groups["y1"].Value);
+        _y2 = int.Parse(match.Groups["y2"].Value);
+
+        if (_y2 > _y1)
+        {
+            (_y2, _y1) = (_y1, _y2);
+        }
+    }
+
+    public override Output Run()
+    {
+        //int maxDownwardSpeed =
+
+        int? found = int.MinValue;
+        int maxY = 0;
+        
+        for (int dY = 0; dY < 10000; dY++)
+        {
+            var (success, y) = Simulate(0, dY);
+            if (success)
+            {
+                found = dY;
+                maxY = y;
+            }
+        }
+
+        if (!found.HasValue)
+        {
+            throw new Exception();
+        }
+        
+        return new Answer(maxY, found.Value);
+    }
+
+    private (bool, int) Simulate(int dX, int dY)
+    {
+        int x = 0;
+        int y = 0;
+
+        int maxY = 0;
+        
+        while (true)
+        {
+            x += dX;
+            y += dY;
+            maxY = Math.Max(y, maxY);
+
+            if (y <= _y1 && y >= _y2)
+            {
+                return (true, maxY);
+            }
+            else if (y < _y2)
+            {
+                return (false, maxY);
+            }
+            
+            dX = Math.Max(0, Math.Sign(dX) * (Math.Abs(dX) - 1));
+            dY--;
+        }
+    }
+
+    private record Answer(long Value, int DeltaY) : Output(Value);
+}
---
+++ Thorarin.AdventOfCode/Year2021/Day17B.cs
@@ -0,0 +1,109 @@
+﻿using System.Text.RegularExpressions;
+using Thorarin.AdventOfCode.Framework;
+
+namespace Thorarin.AdventOfCode.Year2021;
+
+[Puzzle(Year = 2021, Day = 17, Part = 2)]
+public class Day17B : Puzzle
+{
+    private int _x1;
+    private int _x2;
+    private int _y1;
+    private int _y2;
+
+    public override Output SampleExpectedOutput => 112;
+
+    public override Output ProblemExpectedOutput => 2040;
+
+    public override void ParseInput(TextReader reader)
+    {
+        var line = reader.ReadLine()!;
+        var match = Regex.Match(line, "^target area: x=(?<x1>-?\\d+)\\.\\.(?<x2>-?\\d+), y=(?<y1>-?\\d+)\\.\\.(?<y2>-?\\d+)");
+
+        _x1 = int.Parse(match.Groups["x1"].Value);
+        _x2 = int.Parse(match.Groups["x2"].Value);
+        _y1 = int.Parse(match.Groups["y1"].Value);
+        _y2 = int.Parse(match.Groups["y2"].Value);
+
+        if (_y2 > _y1)
+        {
+            (_y2, _y1) = (_y1, _y2);
+        }
+        
+        if (_x2 < _x1)
+        {
+            (_x2, _x1) = (_x1, _x2);
+        }
+    }
+
+    public override Output Run()
+    {
+        int found = 0;
+        var possibleX = Enumerable.Range((int)Math.Sqrt(_x1), _x2).Where(SimulateX).ToList();
+        var possibleY = Enumerable.Range(_y2, 500).Where(SimulateY).ToList();
+
+        foreach (int dX in possibleX)
+        {
+            foreach (int dY in possibleY)
+            {
+                if (Simulate(dX, dY))
+                {
+                    found++;
+                }
+            }
+        }
+        
+        return found;
+    }
+
+    private bool Simulate(int dX, int dY)
+    {
+        int x = 0;
+        int y = 0;
+        
+        while (true)
+        {
+            x += dX;
+            y += dY;
+
+            if (x < _x1 && dX == 0) return false;
+            if (x > _x2) return false;
+            if (y < _y2) return false;
+
+            if (y <= _y1 && y >= _y2 && x >= _x1 && x <= _x2)
+            {
+                return true;
+            }
+
+            dX -= Math.Sign(dX);
+            dY--;
+        }
+    }
+    
+    private bool SimulateX(int dX)
+    {
+        int x = 0;
+
+        while (true)
+        {
+            x += dX;
+            if (x < _x1 && dX == 0) return false;
+            if (x > _x2) return false;
+            if (x >= _x1 && x <= _x2) return true;
+            dX -= Math.Sign(dX);
+        }
+    }
+    
+    private bool SimulateY(int dY)
+    {
+        int y = 0;
+        while (y >= _y2)
+        {
+            y += dY;
+            if (y <= _y1 && y >= _y2) return true;
+            dY--;
+        }
+
+        return false;
+    }
+}
