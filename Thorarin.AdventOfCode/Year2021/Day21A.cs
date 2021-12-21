ba18ecfda3cb4d1fb2f833d61c51c167c3bf5e9f
Marcel Veldhuizen <marcel@subbot.net>
2021-12-21 10:51:40 +0100

    Add (non-optimized) solutions to day 21

---
+++ Thorarin.AdventOfCode/Year2021/Day21A.cs
@@ -0,0 +1,66 @@
+﻿using Thorarin.AdventOfCode.Framework;
+
+namespace Thorarin.AdventOfCode.Year2021;
+
+[Puzzle(Year = 2021, Day = 21, Part = 1)]
+public class Day21A : Puzzle
+{
+    private PlayerState[] _players;
+    
+    public override Output SampleExpectedOutput => 739785;
+
+    public override Output ProblemExpectedOutput => 711480;
+
+    public override void ParseInput(TextReader reader)
+    {
+        _players = new PlayerState[2];
+        
+        _players[0] = new PlayerState(int.Parse(reader.ReadLine()!.Split(':')[1]));
+        _players[1] = new PlayerState(int.Parse(reader.ReadLine()!.Split(':')[1]));
+    }
+
+    public override Output Run()
+    {
+        int playerTurn = 0;
+        int roll = 6;
+        int rolls = 0;
+
+        while (_players.All(p => p.Score < 1000))
+        {
+            _players[playerTurn].Move(roll);
+            roll += 9;
+            rolls += 3;
+
+            //Console.WriteLine(" " + _players[playerTurn].Position + " " + _players[playerTurn].Position);
+            
+            playerTurn = (playerTurn + 1) % 2;
+        }
+
+        var loser = _players.First(p => p.Score < 1000);
+
+        return loser.Score * rolls;
+    }
+
+
+    public class PlayerState
+    {
+        public PlayerState(int Position)
+        {
+            this.Position = Position;
+        }
+
+        public int Score { get; private set; }
+        public int Position { get; private set; }
+
+        public void Move(int rolled)
+        {
+            Position = (Position - 1 + rolled) % 10 + 1;
+            Score += Position;
+        }
+
+        public void Deconstruct(out int Position)
+        {
+            Position = this.Position;
+        }
+    }
+}
---
+++ Thorarin.AdventOfCode/Year2021/Day21B.cs
@@ -0,0 +1,104 @@
+﻿using Thorarin.AdventOfCode.Framework;
+
+namespace Thorarin.AdventOfCode.Year2021;
+
+[Puzzle(Year = 2021, Day = 21, Part = 2)]
+public class Day21B : Puzzle
+{
+    private GameState _game;
+    
+    public override Output SampleExpectedOutput => 444356092776315;
+
+    public override Output ProblemExpectedOutput => 265_845_890_886_828;
+
+    public override void ParseInput(TextReader reader)
+    {
+        _game = new GameState();
+        
+        _game.Players[0] = new PlayerState(0, int.Parse(reader.ReadLine()!.Split(':')[1]));
+        _game.Players[1] = new PlayerState(0, int.Parse(reader.ReadLine()!.Split(':')[1]));
+    }
+
+    public override Output Run()
+    {
+      var result = _game.TakeTurn();
+  
+      return Math.Max(result.Item1, result.Item2);
+    }
+
+    public record GameState()
+    {
+        private static readonly int[] _chances = { 0, 0, 0, 1, 3, 6, 7, 6, 3, 1 };
+        
+        public PlayerState[] Players { get; init; } = new PlayerState[2];
+
+        public int PlayerTurn { get; init; }
+
+        public long Multiplier { get; init; } = 1;
+        
+        public (long, long) TakeTurn()
+        {
+            var w = GetWins();
+            if (w.HasValue)
+            {
+                return w.Value;
+            }
+            
+            long player1 = 0;
+            long player2 = 0;
+            
+            for (int roll = 3; roll <= 9; roll++)
+            {
+                var newState = Play(roll, _chances[roll]);
+                var wins = newState.TakeTurn();
+
+                player1 += wins.Item1;
+                player2 += wins.Item2;
+            }
+
+            return (player1, player2);
+        }
+
+        private GameState Play(int roll, long multiplier)
+        {
+            var state = new[] { Players[0], Players[1] };
+
+            state[PlayerTurn] = state[PlayerTurn].Move(roll);
+
+            return this with
+            {
+                Players = state,
+                PlayerTurn = (PlayerTurn + 1) % 2,
+                Multiplier = Multiplier * multiplier
+            };
+        }
+
+        public (long, long)? GetWins()
+        {
+            if (Players[0].Score >= 21)
+            {
+                return (Multiplier, 0);
+            }
+
+            if (Players[1].Score >= 21)
+            {
+                return (0, Multiplier);
+            }
+
+            return null;
+        }
+    }
+        
+    public record PlayerState(int Score, int Position)
+    {
+        public PlayerState Move(int rolled)
+        {
+            int newPosition = (Position - 1 + rolled) % 10 + 1;
+            return this with
+            {
+                Position = newPosition,
+                Score = Score + newPosition
+            };
+        }
+    }
+}
