d4734cb0285bb575d652c18ccfda5e2366ca18f8
Marcel Veldhuizen <marcel@subbot.net>
2021-12-23 00:59:12 +0100

    Improve speed for day 22 part 2 some more
    Around the 10 ms mark now, not bad.

--- Thorarin.AdventOfCode/Year2021/Day22/Reactor.cs
+++ Thorarin.AdventOfCode/Year2021/Day22/Reactor.cs
@@ -13,16 +13,22 @@ internal class Reactor
 
     public void TurnOn(Cuboid cuboid)
     {
-        var intersects = Cuboids.Where(cuboid.Intersects).ToList();
-        
-        if (intersects.Any(cuboid.IsFullyInside)) return;
-        
-        // Remove existing cuboids fully inside this new cuboid,
-        // because they will be redundant after adding this one.
-        foreach (var redundant in intersects.Where(x => x.IsFullyInside(cuboid)).ToList())
+        List<Cuboid> intersects = new();
+        for (var i = Cuboids.Count - 1; i >= 0; i--)
         {
-            Cuboids.Remove(redundant);
-            intersects.Remove(redundant);
+            var existing = Cuboids[i];
+            if (!existing.Intersects(cuboid)) continue;
+            if (cuboid.IsFullyInside(existing)) return;
+            if (existing.IsFullyInside(cuboid))
+            {
+                // Remove existing cuboids fully inside this new cuboid,
+                // because they will be redundant after adding this one.                
+                Cuboids.RemoveAt(i);
+            }
+            else
+            {
+                intersects.Add(existing);
+            }
         }
         
         if (intersects.Count > 0)
@@ -61,30 +67,13 @@ internal class Reactor
 
     public void TurnOff(Cuboid cuboid)
     {
-        // Exit if no cubes are turned on in the cuboid area
-        if (!Cuboids.Any(cuboid.Intersects)) return;
-
-        // Temporarily remove all cuboids that intersect with
-        // the cuboid we're trying to turn off.
-        List<Cuboid> overlappingCuboids = new List<Cuboid>();
         for (var i = Cuboids.Count - 1; i >= 0; i--)
         {
             var existing = Cuboids[i];
             if (existing.Intersects(cuboid))
             {
                 Cuboids.RemoveAt(i);
-                overlappingCuboids.Add(existing);
-            }
-        }
-
-        foreach (var overlappingCuboid in overlappingCuboids)
-        {
-            // Split the overlapping cuboid into smaller ones,
-            // omitting the intersection with the cuboid we're turning off.
-            // Re-add the remaining cuboids.
-            foreach (var splitCubes in overlappingCuboid.Remove(cuboid))
-            {
-                TurnOn(splitCubes);
+                Cuboids.AddRange(existing.Remove(cuboid));
             }
         }
     }
