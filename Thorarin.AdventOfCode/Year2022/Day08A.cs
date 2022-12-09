a3f4892c021e79384f0d37d2822be8e366516655
Marcel Veldhuizen <marcel@subbot.net>
2022-12-09 08:54:14 +0100

    Add fairly horrible solutions for day 8 (I didn't have any time)

---
+++ Thorarin.AdventOfCode/Year2022/Day08A.cs
@@ -0,0 +1,90 @@
+﻿using MoreLinq;
+using Thorarin.AdventOfCode.Framework;
+
+namespace Thorarin.AdventOfCode.Year2022;
+
+[Puzzle(Year = 2022, Day = 8, Part = 1)]
+public class Day08A : Puzzle
+{
+    private Pos[,] _grid;
+    private int _width;
+    private int _height;
+
+    public override void ParseInput(TextReader reader)
+    {
+        var lines = reader.AsLines().ToList();
+
+        _width = lines[0].Length;
+        _height = lines.Count;
+
+        _grid = new Pos[_width, _height];
+
+        for (int y = 0; y < _height; y++)
+        {
+            for (int x = 0; x < _width; x++)
+            {
+                _grid[x, y] = new Pos(lines[y][x] - 48);
+            }
+        }
+    }
+
+    public override Output SampleExpectedOutput => 21;
+
+    public override Output ProblemExpectedOutput => 1538;
+
+    public override Output Run()
+    {
+        for (int x = 0; x < _width; x++)
+        {
+            int height = -1;
+            for (int y = 0; y < _height; y++)
+            {
+                if (_grid[x, y].Height > height)
+                {
+                    _grid[x, y].Visible = true;
+                    height = _grid[x, y].Height;
+                }
+            }
+
+            height = -1;
+            for (int y = _height - 1; y >= 0; y--)
+            {
+                if (_grid[x, y].Height > height)
+                {
+                    _grid[x, y].Visible = true;
+                    height = _grid[x, y].Height;
+                }
+            }
+        }
+
+        for (int y = 0; y < _height; y++)
+        {
+            int height = -1;
+            for (int x = 0; x < _width; x++)
+            {
+                if (_grid[x, y].Height > height)
+                {
+                    _grid[x, y].Visible = true;
+                    height = _grid[x, y].Height;
+                }
+            }
+
+            height = -1;
+            for (int x = _width - 1; x >= 0; x--)
+            {
+                if (_grid[x, y].Height > height)
+                {
+                    _grid[x, y].Visible = true;
+                    height = _grid[x, y].Height;
+                }
+            }
+        }
+
+        return _grid.Flatten().Count(p => ((Pos)p).Visible);
+    }
+
+    public record Pos(int Height)
+    {
+        public bool Visible { get; set; }
+    }
+}
---
+++ Thorarin.AdventOfCode/Year2022/Day08B.cs
@@ -0,0 +1,87 @@
+﻿using MoreLinq;
+using Thorarin.AdventOfCode.Framework;
+
+namespace Thorarin.AdventOfCode.Year2022;
+
+[Puzzle(Year = 2022, Day = 8, Part = 2)]
+public class Day08B : Puzzle
+{
+    private Pos[,] _grid;
+    private int _width;
+    private int _height;
+
+    public override void ParseInput(TextReader reader)
+    {
+        var lines = reader.AsLines().ToList();
+
+        _width = lines[0].Length;
+        _height = lines.Count;
+
+        _grid = new Pos[_width, _height];
+
+        for (int y = 0; y < _height; y++)
+        {
+            for (int x = 0; x < _width; x++)
+            {
+                _grid[x, y] = new Pos(lines[y][x] - 48);
+            }
+        }
+    }
+
+    public override Output SampleExpectedOutput => 8;
+
+    public override Output ProblemExpectedOutput => 496125;
+
+    public override Output Run()
+    {
+        for (int x = 0; x < _width; x++)
+        {
+            for (int y = 0; y < _height; y++)
+            {
+                _grid[x, y].Score = CalculateScore(x, y);
+            }
+        }
+
+        return _grid.Flatten().Max(p => ((Pos)p).Score);
+    }
+
+    private int CalculateScore(int x, int y)
+    {
+        int height = _grid[x, y].Height;
+
+        int left = 0;
+        for (int i = x - 1; i >= 0; i--)
+        {
+            left++;
+            if (_grid[i, y].Height >= height) break;
+        }
+
+        int right = 0;
+        for (int i = x + 1; i < _width; i++)
+        {
+            right++;
+            if (_grid[i, y].Height >= height) break;
+        }
+
+        int up = 0;
+        for (int i = y - 1; i >= 0; i--)
+        {
+            up++;
+            if (_grid[x, i].Height >= height) break;
+        }
+
+        int down = 0;
+        for (int i = y + 1; i < _height; i++)
+        {
+            down++;
+            if (_grid[x, i].Height >= height) break;
+        }
+
+        return left * right * up * down;
+    }
+
+    public record Pos(int Height)
+    {
+        public int Score { get; set; }
+    }
+}
